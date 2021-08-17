1a390485a09d69adda2db4ed7755c15463c195ed
SofiaMejia <A01252540@itesm.mx>
2021-08-17 23:45:29 +0000

    15 escalera

--- assignments/15Escalera/src/exercise.py
+++ assignments/15Escalera/src/exercise.py
@@ -1,6 +1,9 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-
+    casa = float(input('Altura de la casa: '))
+    angulo = int(input('Angulo en grados: '))
+    largo = round(casa / math.sin(math.radians(angulo))) 
+    print('Largo de la escalera: '+str(largo))
 if __name__ == '__main__':
     main()
